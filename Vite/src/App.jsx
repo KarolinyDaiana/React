66294b8dda13ff4b3ea1ab01dc753cf8c4ae429c
KarolinyDaiana <karoliny.cieply@gmail.com>
2023-10-09 19:23:24 -0300

    novo

---
+++ Vite/src/App.jsx
@@ -0,0 +1,17 @@
+import Banner from "./components/Banner";
+import Formulario from "./components/Formulario";
+
+const App = () => {
+  return (
+    <div>
+    
+      <h1>App React</h1>
+
+      <Banner />
+      <Formulario />
+
+    </div>
+  )
+};
+
+export default App;
---
+++ Vite/src/components/Formulario/index.jsx
@@ -0,0 +1,19 @@
+import './Formulario.css';
+import CampoTexto from '../CampoTexto';
+
+const Formulario = () => {
+    return (
+        <section className='formulario'>
+
+            <form>
+                <h2>Preencha os campos para a compra:</h2>
+                <CampoTexto label="Nome" placeholder="Digite o seu nome"/>
+                <CampoTexto label="Sobrenome" placeholder="Digite o seu sobrenome"/>
+                <button>Cadastrar</button>
+            </form>
+        </section>
+
+    )
+}
+
+export default Formulario;
